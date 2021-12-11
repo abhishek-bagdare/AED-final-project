58eff263fb67f663f802ae113aa42b178daa255a
Abhishek P Bagdare <90578089+abhishek-bagdare@users.noreply.github.com>
2021-12-11 00:52:41 -0500

    Added Rehabilitation Enterprise

---
+++ src/business/Enterprise/RehabilitationEnterprise.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Enterprise;
+
+import Business.Role.Role;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author abhishekbagdare
+ */
+public class RehabilitationEnterprise extends Enterprise {
+
+    public RehabilitationEnterprise(String name, EnterpriseType type) {
+         super(name,EnterpriseType.Rehabilitation);
+    }
+
+    @Override
+    public ArrayList<Role> getSupportedRole() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
